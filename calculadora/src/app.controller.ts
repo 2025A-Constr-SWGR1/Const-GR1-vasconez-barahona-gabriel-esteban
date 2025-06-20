990d24433cab0d142ec84edb82979c7941aeb1bf
Gabo <gabriel.vasconez@epn.edu.ec>
2025-06-20 16:00:12 -0500

    Clase 20-06-2025

--- calculadora/src/app.controller.ts
+++ calculadora/src/app.controller.ts
@@ -1,9 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('database')
+  getDatabaseInformation(): object {
+    return {
+      DB_HOST: this.configService.get('DB_HOST'),
+      DB_USER: this.configService.get('DB_USER'),
+      DB_PASSWORD: this.configService.get('DB_PASSWORD'),
+      DB_NAME: process.env.DB_NAME
+    }
+  }
 
   @Get()
   getHello(): string {
